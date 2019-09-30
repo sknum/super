840be0ee51d49bc8231257f6435c124a5407ecec
sknum <53473560+sknum@users.noreply.github.com>
2019-09-30 03:43:22 -0700

    Update micro.c

--- micro.c
+++ micro.c
@@ -1,44 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include<stdio.h>
 int main()
- int t=0,c=0,d=0;
-scanf("%d",&t);
-if(t<=0)
-{ 
-  while(c<t)
 {
-  int k,n,i=0,a[1000],small=0;
-  scanf("%d",&n);
-  scanf("%d",&k);
-  for(i=0;i<n;i++)
- {
-   scanf("%d",&a[i]);
+    int t,n,i,min=10,k;
+    printf("enter test case:\n");
+    scanf("%d",&t);
+    while(t--)
+    {
+     min=10;
+     printf("enter size of array AND elements \n");
+     scanf("%d%d",&n,&k);
+     int a[n];
+     for(i=0;i<n;i++)
+     {
+      scanf("%d",&a[i]);
+      if(a[i]<min)
+       min=a[i];
+     }
+   
+     if(min>=k)
+      printf("0\n");
+     else
+      printf("%d\n",k-min);
+    }
+    return 0;
 
- }
-small=a[0];
-for(i=0;i<n;i++)
-{
-  if(a[i]<small)
-  {
-    small=a[i];
-  }
 }
-if(small==k)
-{
-  printf("%d",d);
-}
-}
-else
-{
-  while(small<k)
-  {
-    small++;
-    d++;
-  }
-printf{("%d",d);
-small=0;
-d=0;
-c++;
-}
-}
+
